8f04eae4dfa0e129233bab13003206658a3d48f6
Valerie <finwings@gmail.com>
2019-06-22 15:31:03 -0400

    In order to change Default Headre image, an assets folder was created, header.php copied to child theme, and a snippet of code added to functions.php file;screenshot image added to child theme

--- wp-content/themes/child_of_2017/functions.php
+++ wp-content/themes/child_of_2017/functions.php
@@ -11,9 +11,16 @@ style.css' );
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
 
-
+// 4 google fonts are added: Arimo,Niconne,Teko, Montserrat
 function wpb_add_google_fonts() {
 wp_enqueue_style( 'wpb-google-fonts','https://fonts.googleapis.com/css?family=Arimo|Montserrat|Niconne|Teko&display=swap', false );
 }
 add_action( 'wp_enqueue_scripts',
 'wpb_add_google_fonts' );
+
+//the following snippet of code was taken from Kinsta.com to replace default Header image
+function my_custom_header_args( $args ) {
+  $args['default-image'] = get_theme_file_uri( '/assets/images/header.jpg' );
+  return $args;
+}
+add_filter( 'twentyseventeen_custom_header_args', 'my_custom_header_args' );
